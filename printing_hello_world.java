215ba2e263936eb1b2cf418a84a4c90d5e869f4e
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2022-02-17 12:06:25 +0530

    Create printing_hello_world.java
    java programming

---
+++ printing_hello_world.java
@@ -0,0 +1,13 @@
+//Program to printing Hello, World
+
+// Importing modules
+import java.lang.*;
+
+// Creating a class as public
+public class printing_hello_world {
+      //printing String 
+      System.out.println("Hello, World");
+}
+
+//Thanks for view my repository
+//bye
