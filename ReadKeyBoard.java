f096d64f68e30ff9729c542564480ae0fc896f5d
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2022-02-21 15:25:26 +0530

    write a program to read keywords from keyboard using scanner

---
+++ ReadKeyBoard.java
@@ -0,0 +1,20 @@
+package com.MyJavaCourse;
+
+// i am going to show you how to scanner read keywords form keyboard
+// write a program to read keywords from keyboard using scanner
+
+//import modules
+import java.util.Scanner;
+
+
+
+public class ReadKeyBoard {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //The nextInt() method of Java Scanner class is used to scan the next token of the input as an int.
+        int x=sc.nextInt();
+        System.out.println(x);
+
+    }
+}
