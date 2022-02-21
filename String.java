6a88091dae119196d822f53f9d42a2641b71b637
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2022-02-21 15:57:12 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,14 @@
+mport java.lang.*;
+import java.util.*;
+public class String {
+
+    public static void main(String[] args) {
+        String name;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("May I know your Name: ");
+        name=sc.readLine();
+        System.out.println("Welcome "+name);
+    }
+
+}
