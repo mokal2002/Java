797c7d9c29933a08c6971d5f9713883b3e761364
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2022-02-22 18:08:56 +0530

    Create Literals.java
    //Literals in java

---
+++ Literals.java
@@ -0,0 +1,28 @@
+//Literals in java
+
+public class Literals {
+
+    public static void main(String[] args) {
+        
+        
+        long l=9999999999L;  //The long data type is a 64-bit two's complement integer.00L
+        float f=12.56f;      //The default value of a float in Java is 0.0f.00.00f
+        double d=12.56d;     //00.00d
+        
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+    
+}
+
+
+
+/////////Output/////////
+// 9999999999         //
+// 12.56              //
+// 12.56              //
+////////////////////////
+
+//Thanks You 
+//bye
