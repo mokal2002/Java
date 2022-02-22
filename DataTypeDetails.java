bceb77e535ad0f9e89a78282dc7483a0eadb5b94
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2022-02-22 18:17:14 +0530

    //Write a program to display Details of a data type
    //Write a program to display Details of a data type

---
+++ DataTypeDetails.java
@@ -0,0 +1,30 @@
+//Write a program to display Details of a data type
+
+//import modules
+import java.awt.*;
+import javax.swing.*;
+import java.lang.*;
+
+
+public class DataTypeDetails {
+
+    public static void main(String[] args) {
+        int x=-5;//5
+        
+        System.out.println(Integer.toBinaryString(x));
+        //The java.lang.Integer.toBinaryString() method returns a string representation of the integer argument as an unsigned integer in base 2. It accepts an argument in Int data-type and returns the corresponding binary string.
+    }
+    
+}
+
+
+
+//////////////////Output///////////////////
+//                                       //
+//   11111111111111111111111111111011    //
+//                                       //
+///////////////////////////////////////////
+
+
+//Thanks you
+//bye
