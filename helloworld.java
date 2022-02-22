238c811c6108cda54d8ff41faf57ee5fe5707539
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2022-02-22 16:31:37 +0530

    Update and rename printing_hello_world.java to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,7 @@
 import java.lang.*;
 
 // Creating a class as public
-public class printing_hello_world {
+public class helloworld {
       public static void main(String[] args) {
             //printing String 
            System.out.println("Hello, World");
